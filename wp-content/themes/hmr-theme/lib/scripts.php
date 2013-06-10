01ac33dd35691d17de961b437ba1835d846e29a8
Jon Bukiewicz <jonathan@tenebroso.net>
2013-06-10 15:38:07 -0500

    no message

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '99cb91527a57abdfbe15a631aa0b6f3e');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0ff9df4bdca8c1be21b05e0474d94dc1');
 
   // Load style.css from child theme
   if (is_child_theme()) {
