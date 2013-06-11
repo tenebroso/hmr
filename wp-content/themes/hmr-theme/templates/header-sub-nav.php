f658400433f64b98703bea3b0ae5f96dd803c2cf
Jonathan Bukiewicz <jonathan@tenebroso.net>
2013-06-11 16:47:17 -0500

    Sub-Nav Tweaks

--- wp-content/themes/hmr-theme/templates/header-sub-nav.php
+++ wp-content/themes/hmr-theme/templates/header-sub-nav.php
@@ -3,7 +3,7 @@
 
 	<div class="container">
 
-	<?php if(is_page(array('27','29'))) { 
+	<?php if(is_page(array('27')) || is_post_type_archive('team') ) { 
 		wp_nav_menu( array('menu' => 'Who We Are Sub-Nav' ));
 	} ?>
 
