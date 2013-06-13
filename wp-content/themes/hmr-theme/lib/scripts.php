f6ac8ad7bd6018ddaa4041a442625ee199aed224
Jon Bukiewicz <jonathan@tenebroros.net>
2013-06-13 18:51:09 -0500

    Homepage tweaks using CSS calc to define positioning. Still building framework for these animations.

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '382377f7dfea77d2af863a3a9ad03a07');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8c63d11f7abddac83946ced9f376c339');
 
   // Load style.css from child theme
   if (is_child_theme()) {
