6f56bb9b85d184e22e5ddc2e869078b7d8c126b8
Jon Bukiewicz <jonathan@tenebroros.net>
2013-06-13 20:35:56 -0500

    Remove unnecessary bootstrap JS files

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5242d29fb471511110a3a3ae9f088797');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a4976630627cbb69351539749617c0c3');
 
   // Load style.css from child theme
   if (is_child_theme()) {
