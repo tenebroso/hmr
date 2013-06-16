7b538c22e21b8ad871b1ed84c0c7c3d746c3eadb
Jon Bukiewicz <jonathan@tenebroros.net>
2013-06-16 18:53:26 -0500

    Convert to SVG based Logo. Other elements when applicable. Preserve PSD based PNG for rasterized elements not provided as vector.

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'fecbc696fd36ad7099a503f17a382c63');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f07b47bf112f28fc2c753cbe90a4f0c3');
 
   // Load style.css from child theme
   if (is_child_theme()) {
