51e0f51497b4075a0ff1c3a848bf2d302257aba4
Jonathan Bukiewicz <jonathan@tenebroso.net>
2013-06-20 09:39:52 -0500

    Add traditional thumbnail back to team landing page for previewing.

--- wp-content/themes/hmr-theme/templates/content-team.php
+++ wp-content/themes/hmr-theme/templates/content-team.php
@@ -2,9 +2,7 @@
 <?php $image_url = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'thumbnail'); ?>
 
 <article class="span2">
-	<div class="unhide" style="display:none">
-  		<div style="background: url(<?php echo $image_url[0]; ?>)"><a href="<?php the_permalink();?>"></a></div>
-	</div>
+	<?php the_post_thumbnail('thumbnail');?>
    	<p class="circe large">
    		<a href="<?php the_permalink();?>"><?php the_title();?><br />
    			<em class="arvo"><?php the_field('title');?></em>
