a62cee3cd614d8aba7d82f678457ab9bdb0832bd
Jonathan Bukiewicz <jonathan@tenebroso.net>
2013-06-20 15:50:10 -0500

    Fixes to homepage background slideshow, per call.

--- wp-content/themes/hmr-theme/assets/js/views/home/homepageImageViewer.js
+++ wp-content/themes/hmr-theme/assets/js/views/home/homepageImageViewer.js
@@ -1,59 +1,21 @@
 //HMR.Home = "xxx";
  
- var HMR = HMR || {};
+var HMR = HMR || {};
 
 ;(function() {
 
   HMR.homepageImageViewer = function() {
 
-    $.backstretch([
-      "/assets/CORPORATE-8.jpg"//, 
-      //"/assets/ART-AND-GRAPHIC-16.jpg", 
-      //"/assets/FABRIC-5.jpg",
-      //"/assets/FABRIC-11.jpg",
-      //"/assets/INNOVATION-13.jpg",
-      //"/assets/SOCIAL-5.jpg",
-      //"/assets/SOCIAL-16.jpg",
-      //"/assets/WEDDINGS-2.jpg",
-      //"/assets/WEDDINGS-22.jpg"
-    ], {duration: 5000, fade: 1250});
-
-    $('.forward').transition({ delay:1000, opacity: 100, marginLeft: '100px'}, 750, 'ease');
-    $('.thinkers').transition({ delay:1250, opacity: 100, marginLeft: '150px'}, 1000, 'easeOutCirc');
-    //$('.forward').transition({ delay:3950, opacity: 0, marginLeft: '350px'}, 500, 'ease');
-    //$('.thinkers').transition({ delay:3950, opacity: 0, marginLeft: '350px'}, 500, 'easeOutCirc');
-
-
-    //$('.forward2').transition({ delay:7500, opacity: 100, marginRight: '100px'}, 500, 'ease');
-    //$('.thinkers2').transition({ delay:7500, opacity: 100, marginRight: '150px'}, 750, 'easeOutCirc');
-    //$('.forward2').transition({ delay:3250, opacity: 0, marginLeft: '-50px'}, 500, 'ease');
-    //$('.thinkers2').transition({ delay:3250, opacity: 0, marginLeft: '-80px'}, 500, 'easeOutCirc');
-
-    //$('.forward3').transition({ delay:14250, opacity: 100, marginRight: '100px'}, 500, 'ease');
-    //$('.thinkers3').transition({ delay:14250, opacity: 100, marginRight: '150px'}, 750, 'easeOutCirc');
-    //$('.forward3').transition({ delay:3250, opacity: 0, marginLeft: '-50px'}, 500, 'ease');
-    //$('.thinkers3').transition({ delay:3250, opacity: 0, marginLeft: '-80px'}, 500, 'easeOutCirc');
-
-
-
-     // Create an array of images that you'd like to use
-    //var images = [
-      //"/assets/CORPORATE-8.jpg",
-      //"/assets/ART-AND-GRAPHIC-16.jpg", 
-      //"/assets/FABRIC-5.jpg",
-      //"/assets/FABRIC-11.jpg",
-      //"/assets/INNOVATION-13.jpg",
-      //"/assets/SOCIAL-5.jpg",
-      //"/assets/SOCIAL-16.jpg",
-      //"/assets/WEDDINGS-2.jpg",
-      //"/assets/WEDDINGS-22.jpg"
-    //];
+    // Image path
+    var img = '/assets/CORPORATE-8.jpg';
     
-    // Get a random number between 0 and the number of images
-    //var randomNumber = Math.floor( Math.random() * images.length );
+    
+    // Setup backstretch
+    $.backstretch([img]);
+
+    $('.forward').transition({delay:1000, opacity: 1, marginLeft: '100px'}, 750, 'ease');
+    $('.thinkers').transition({ delay:1250, opacity: 1, marginLeft: '150px'}, 1000, 'easeOutCirc');
  
-    // Use the random number to load a random image
-    //$.backstretch(images[randomNumber], {duration: 5000, fade: 1250});
 
   };
 
