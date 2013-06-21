e9ad1c1c80aaf011c2e36548ea4f230ccdb1fb3d
Jon Bukiewicz <jonathan@tenebroros.net>
2013-06-21 13:31:55 -0500

    Attempted fixes for top nav hide/show

--- wp-content/themes/hmr-theme/assets/js/views/nav/navSlideToggle.js
+++ wp-content/themes/hmr-theme/assets/js/views/nav/navSlideToggle.js
@@ -6,40 +6,37 @@ var HMR = HMR || {};
 
     var sliderHeight = "30px";
  
-    $(document).ready(function(){
-        $('.scrollTop').each(function () {
-                    var current = $(this);
-                    current.attr("box_h", current.height());
-                }
+        $(".scrollTop").each(function () 
+            {
+                var current = $(this);
+                current.attr("box_h", current.height());
+            }
+        );
+        $(".slideUp").click(function() { closeSlider() });
+        $(".slideDown").click(function() { openSlider() });
      
-         );
-        $(".slideToggle").click(function() { closeSlider() })
-     
-    });
      
     function openSlider()
      
     {
         var open_height = $(".scrollTop").attr("box_h") + "px";
         $(".scrollTop").animate({"height": open_height}, {duration:250 });
-        $(".slideToggle").removeClass('slideUp');
-        $('.slideToggle').addClass('slideDown');
-        $('.banner').slideDown(250);
-        $('.sub-nav').transition({ y:0}, 250, 'ease');
-        $('.slideDown').transition({ y:0}, 250, 'ease');
-        $(".slideDown").click(function() { closeSlider() });
+        $(".banner").slideDown(250);
+        $(".sub-nav").transition({ y:0}, 250, "ease");
+        $(".slideDown").transition({ y:0}, 250, "ease");
+        $(".slideToggle").removeClass("slideUp");
+        $(".slideToggle").addClass("slideDown");
     }
      
     function closeSlider()
      
     {
         $(".scrollTop").animate({"height": sliderHeight}, {duration: 250 });
-        $(".slideToggle").removeClass('slideDown');
-        $('.slideToggle').addClass('slideUp');
-        $('.banner').slideUp(250);
-        $('.sub-nav').transition({ y:-9}, 250, 'ease');
-        $('.slideUp').transition({ y:-14}, 250, 'ease');
-        $(".slideUp").click(function() { openSlider() });
+        $(".banner").slideUp(250);
+        $(".sub-nav").transition({ y:-9}, 250, "ease");
+        $(".slideUp").transition({ y:-14}, 250, "ease");
+        $(".slideToggle").removeClass("slideDown");
+        $(".slideToggle").addClass("slideUp");
     }
 
   };
--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -35,7 +35,7 @@ function roots_scripts() {
   }
 
   wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.6.2.min.js', false, null, false);
-  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, '1c5ff684cf9a3dcd700f026397a65646', true);
+  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, '22e010cdc6ac52f454b4cc03987db906', true);
   wp_enqueue_script('modernizr');
   wp_enqueue_script('jquery');
   wp_enqueue_script('roots_scripts');
