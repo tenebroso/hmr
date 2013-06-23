adaae6d1b74e0eb1143942b443523613445535b6
Jon Bukiewicz <jonathan@tenebroros.net>
2013-06-23 09:23:14 -0500

    CSS Fixes

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a733287e3a375c19dd619a1f1b1cc7bf');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '78872908f730ada9c02efb042afd0305');
 
   // Load style.css from child theme
   if (is_child_theme()) {
