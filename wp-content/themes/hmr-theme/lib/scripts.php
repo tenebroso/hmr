a9269b291ae4be136d8d4b2b887ac4fb164e67a6
Jon Bukiewicz <jonathan@tenebroso.net>
2013-06-23 22:25:46 -0500

    Homepage and mobile nav CSS fixes

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0434caa703d0dd8f516cc828f3bfaac9');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c3a268861af1b312fa8cb7a508d0df7c');
 
   // Load style.css from child theme
   if (is_child_theme()) {
