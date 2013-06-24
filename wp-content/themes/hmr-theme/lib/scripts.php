994beae25331b211496893effaa1db8e7d159822
Jon Bukiewicz <jonathan@tenebroso.net>
2013-06-23 22:49:11 -0500

    Focus states for connect form

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd910902eec574f835c8014ae76884c9f');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c7a0a14b87fcf1ce858be5aa7aad8ad8');
 
   // Load style.css from child theme
   if (is_child_theme()) {
