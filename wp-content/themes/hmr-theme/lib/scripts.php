f15967c13ad336b993f7a36ab0abee8b8a6595c0
Jon Bukiewicz <jonathan@tenebroros.net>
2013-06-29 16:53:51 -0500

    Fix for footer & mobile nav to remove + and >>

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd98b4db153ba32678cc643cd8b5028da');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b22a4a24c1c820f7e2a63cf52831e591');
 
   // Load style.css from child theme
   if (is_child_theme()) {
