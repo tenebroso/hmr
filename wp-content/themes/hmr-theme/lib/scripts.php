a7f07a47d1eff6d8a24ceb247bc090aa30e67b8f
Jon Bukiewicz <jonathan@tenebroros.net>
2013-07-03 14:42:23 -0500

    Team Landing Page - adjust animation of tooltips over faces, add capabilities and links to the names in the tooltips

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6466e7ebea7634c4be81f558ca2b5a3d');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f1ab773bf68c3c70df27cfd0a255f11c');
 
   // Load style.css from child theme
   if (is_child_theme()) {
@@ -40,7 +40,7 @@ function roots_scripts() {
   }
 
   wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.6.2.min.js', false, null, false);
-  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, '19ffd2e560186e2c14de16722f6e1bd3', true);
+  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, '4857bdabd2294468a1a11c188285f708', true);
 
   wp_enqueue_script('modernizr');
   wp_enqueue_script('jquery');
