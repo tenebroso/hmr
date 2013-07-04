70ec1c7c4722890379e22581e7cc1de67abee6e6
Jon Bukiewicz <jonathan@tenebroros.net>
2013-07-03 22:09:00 -0500

    Set font on capability detail pages, small resolution, to be smaller

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '04848bc6355c95ac96ae8ab5ce89df85');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd37a5b7a64422eabb3f439fbee1f6b02');
 
   // Load style.css from child theme
   if (is_child_theme()) {
