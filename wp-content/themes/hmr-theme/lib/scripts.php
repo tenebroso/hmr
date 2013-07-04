0e15cebde4cc843472cc53cd5c512db66ef6f06c
Jon Bukiewicz <jonathan@tenebroros.net>
2013-07-03 21:47:30 -0500

    Update font calls to be simpler/more straightforward, using font-weight and font-style values rather than specific font names for those styles. Also, update body font to be Circe as requested.

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e7e8e1d772adfb6039bd7bfb170f9227');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '04848bc6355c95ac96ae8ab5ce89df85');
 
   // Load style.css from child theme
   if (is_child_theme()) {
