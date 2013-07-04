49b95bff6d38d2cc7418c473e3340264b4a4de82
Jon Bukiewicz <jonathan@tenebroros.net>
2013-07-03 21:01:37 -0500

    Lighter Version of Circe for Capabilities "View Gallery" rollover, press and testimonials rollover

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9350f901aaccf0fdfed1394c83e6bd73');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e7e8e1d772adfb6039bd7bfb170f9227');
 
   // Load style.css from child theme
   if (is_child_theme()) {
