d5e74b796ec699a36f123dca4f513f796bbeb088
Jon Bukiewicz <jonathan@tenebroros.net>
2013-07-03 20:44:56 -0500

    Add approved, cropped Team photos and Bob Mertzluff Photo to Team and Individual Team Pages, respectively

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1cc120636566fc4f8b2cc246d5fd4304');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'abe01cac464e2fc5bfbd01c95272afa9');
 
   // Load style.css from child theme
   if (is_child_theme()) {
--- wp-content/themes/hmr-theme/templates/content-team.php
+++ wp-content/themes/hmr-theme/templates/content-team.php
@@ -1,10 +1,15 @@
 
-<?php $image_url = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'thumbnail'); ?>
+<?php $image_url = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'thumbnail'); $custom = get_field('custom_thumbnail');?>
 
 <article class="span2">
 	<a href="<?php the_permalink();?>">
 		<span class="hidden-phone">
-			<?php the_post_thumbnail('thumbnail');?>
+			
+			<?php if($custom) { ?>
+				<img src="<?php echo $custom; ?>" class="custom-thumb">
+			<?php } else { ?>
+				<?php the_post_thumbnail('thumbnail');?>
+			<?php } ?>
 		</span>
 	   	<p class="circe large">
 			<span class="name"><?php the_title();?></span>
