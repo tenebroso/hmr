bf44bd1f388733bef564575fdfa6a5d7fb6bb576
Jon Bukiewicz <jonathan@tenebroso.net>
2013-07-04 23:02:49 -0500

    Major Re-Organization of Stylesheets into views, for easier hand-off to WCST and location of specific styles. Also removed unnecessary Bootstrap JS and CSS files.

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd37a5b7a64422eabb3f439fbee1f6b02');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8812ce0bd4b897e86375554883c21cfa');
 
   // Load style.css from child theme
   if (is_child_theme()) {
@@ -40,7 +40,7 @@ function roots_scripts() {
   }
 
   wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.6.2.min.js', false, null, false);
-  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, '01ed4b56ebfda8fdb6b370180fb0c925', true);
+  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, '94891dd7ec3dffabe333b1350649490a', true);
 
   wp_enqueue_script('modernizr');
   wp_enqueue_script('jquery');
