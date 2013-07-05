300776df1ab3d3a1fa5b91248b0c0970df29dc43
Jon Bukiewicz <jonathan@tenebroso.net>
2013-07-05 10:15:46 -0500

    Lots of tiny fixes for mobile

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9e8047ace819b003449ccd9137ed0aee');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f8a644ef78d0af03072c96fa55dd7280');
 
   // Load style.css from child theme
   if (is_child_theme()) {
