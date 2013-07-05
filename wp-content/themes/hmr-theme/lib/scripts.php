f831b35c9fff3f66af139e0ff767785e3dbc4072
Jon Bukiewicz <jonathan@tenebroso.net>
2013-07-05 08:41:26 -0500

    Connect page CSS was accidentally hidden in views/slideshow.less so I moved it all out to views/connect.less and notated that in app.less.

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '703af2f6a106e9c35de0585eeaa82fb9');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f756dad68f60e803e5c7e4d37a4ed048');
 
   // Load style.css from child theme
   if (is_child_theme()) {
