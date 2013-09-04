b8eb360badedd3f871b3d862bc42ac617ebfbf90
Jon Bukiewicz <jonathan@tenebroros.net>
2013-09-04 15:34:28 -0500

    "HMR" Phase II - Basecamp Changes
    Please add super thin border to photo, just like you have around individual headshot photos
    Get rid of “carrot” in the “name box” that pops up
    Individual names in circe font needs to be thinner

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '74946c83b642de0dbd5cf600b8ae534b');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '07ad1bfbe6bd7fa82ec2f9fa5139d4d8');
 
   // Load style.css from child theme
   if (is_child_theme()) {
