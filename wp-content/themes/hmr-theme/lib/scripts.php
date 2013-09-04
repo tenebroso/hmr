9d96baea811e4e8ebbe3f8b8b3fdec74c5ab1959
Jonathan Bukiewicz <jonathan@tenebroso.net>
2013-09-03 20:43:25 -0500

    Connect page changes - form titles in white, remove points of contact

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '027d10856f2fa807de7114355b01dceb');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9cd02a055b494b2fb6b1ea4ebc4281d4');
 
   // Load style.css from child theme
   if (is_child_theme()) {
--- wp-content/themes/hmr-theme/template-connect.php
+++ wp-content/themes/hmr-theme/template-connect.php
@@ -8,8 +8,8 @@ Template Name: Connect
 <?php the_field('lead_form');?>
 
 <hr />
-
-<?php if(get_field('contact')): ?>
+<!-- 
+<?php //if(get_field('contact')): ?>
 
 	<h2 class="caps text-center">Points of Contact</h2>
 
@@ -17,19 +17,19 @@ Template Name: Connect
  
 		<ul class="row-fluid pointsOfContact">
 	 
-		<?php while(has_sub_field('contact')): ?>
+		<?php //while(has_sub_field('contact')): ?>
 	 
-			<li class="span3 points"><?php the_sub_field('contact_information'); ?></li>
+			<li class="span3 points"><?php //the_sub_field('contact_information'); ?></li>
 	 
-		<?php endwhile; ?>
+		<?php //endwhile; ?>
 	 
 		</ul>
 
 	</div>
  
-<?php endif; ?>
+<?php //endif; ?>
 
-<hr />
+<hr /> -->
 
 <?php $secondary = get_field('secondary_form'); if ($secondary) { echo $secondary; } ?>
 
