5c6bb74dce6a9c4593cf6177469c37eeff0f978f
Jon Bukiewicz <jonathan@tenebroros.net>
2013-09-04 09:11:52 -0500

    GLOBAL - font change, nag remove arrow

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c985f98916e637cba6bc2a1b53d1b11e');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f851f7deae7bdfa75d2f36a23ca36f4b');
 
   // Load style.css from child theme
   if (is_child_theme()) {
