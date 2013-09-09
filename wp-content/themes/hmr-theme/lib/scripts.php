c42a299a035ebe4c6dc4d1b05069bf07d849b827
Jon Bukiewicz <jonathan@tenebroros.net>
2013-09-08 19:32:37 -0500

    Add Pinterest Pin It Button for Images for blog post images

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6686dbe0c46efb17222bc85e8b52c517');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6eba9f11f7d0eb7b2273f0456847d983');
 
   // Load style.css from child theme
   if (is_child_theme()) {
