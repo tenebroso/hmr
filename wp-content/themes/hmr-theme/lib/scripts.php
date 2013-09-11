5865cfb1597836189f3cf59353f250dcacc4990d
Jon Bukiewicz <jonathan@tenebroros.net>
2013-09-10 19:58:13 -0500

    Fix the team landing page footer animation

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6e12e5bd878a1799ed88d54baaa0b089');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1d5ae61791f558949ed008e72bd2e321');
 
   // Load style.css from child theme
   if (is_child_theme()) {
