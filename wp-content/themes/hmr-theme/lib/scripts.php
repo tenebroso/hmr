86dd5b7e33e0e142627251acd85a30352c9675fc
Jon Bukiewicz <jonathan@tenebroros.net>
2013-09-10 20:14:19 -0500

    Single capabilities text font weight fix

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1d5ae61791f558949ed008e72bd2e321');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f9d56bf0ab3947f9426d2cf1ed74c346');
 
   // Load style.css from child theme
   if (is_child_theme()) {
