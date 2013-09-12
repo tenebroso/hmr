d6b14150157dace0aace4940f02b60a5be36b3bd
Jonathan Bukiewicz <jonathan@tenebroso.net>
2013-09-12 15:03:47 -0500

    Fixes to blog - capitalization of author name

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e1b4a3a78066817a3d24818d3f1e7f94');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'cc896010b76209d49773a200306452a5');
 
   // Load style.css from child theme
   if (is_child_theme()) {
