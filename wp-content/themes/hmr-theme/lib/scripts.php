f4cb837661f83fea528c64e4507f225a469ec204
Jon Bukiewicz <jonathan@tenebroros.net>
2013-09-13 16:18:24 -0500

    Make footer text/icons slate instead of white

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '028c34e901707e89cc4f35009d6aa273');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '80e001819c965e4da30f1651f1ab697b');
 
   // Load style.css from child theme
   if (is_child_theme()) {
