6cb1d722dc992c5ccdcdee49b266df37d4fc0a11
Jon Bukiewicz <jonathan@tenebroros.net>
2013-09-13 18:36:47 -0500

    Fixes for nav on mobile

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd05d93896418118051a9c877eb017ec9');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '4aecd42d9a31d32701e70dd97fd1429f');
 
   // Load style.css from child theme
   if (is_child_theme()) {
