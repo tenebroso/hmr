c15e7a997651859d85a438f5d183e2a32ee68ec3
Jon Bukiewicz <jonathan@tenebroros.net>
2013-09-25 11:21:07 -0500

    Move phrase 'ever evolving' to the left, fix homepage logo rollover

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '47d7a8777c842b1b3eb82e73f516e8b3');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c31c07f9102c49353c68e798ee47f881');
 
   // Load style.css from child theme
   if (is_child_theme()) {
