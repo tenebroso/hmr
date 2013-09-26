1fcbcb46611d088a791fed9ae95faabd1ef40dd4
Jon Bukiewicz <jonathan@tenebroso.net>
2013-09-26 14:48:12 -0500

    Mobile fixes for iphone/etc

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1470d665fb62d5cfe1e911fd8932d2f7');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd117c591f9985251d775b98edd2b3488');
 
   // Load style.css from child theme
   if (is_child_theme()) {
