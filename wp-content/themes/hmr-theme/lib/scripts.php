22692e589e21d0540223b6c95a5ef0ee4be82f96
Jonathan Bukiewicz <jonathan@tenebroso.net>
2013-09-27 11:30:15 -0500

    Gold pinterest icon

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'fb5877882cbbf11bba5a13b8d13aa112');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1d113fc21d99140d48b56d70a6f62960');
 
   // Load style.css from child theme
   if (is_child_theme()) {
