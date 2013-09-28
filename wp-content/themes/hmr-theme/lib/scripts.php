6d7b6deec7d75572d61685144d41d7e84ddd5bbd
Jon Bukiewicz <jonathan@tenebroros.net>
2013-09-28 14:16:31 -0500

    More nav

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '28970142cb5a73ebfac3f1ce6030c88a');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'adb24e8c2c456eee9070dacca349cd62');
 
   // Load style.css from child theme
   if (is_child_theme()) {
