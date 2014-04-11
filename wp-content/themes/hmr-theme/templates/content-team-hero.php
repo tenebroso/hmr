6f6f3e855dbf85eeadf3c397db5fa856d1bd9893
Jon Bukiewicz <jonathan@tenebroros.net>
2014-04-11 14:22:55 -0500

    Remove Carli Milstein from hover

--- wp-content/themes/hmr-theme/templates/content-team-hero.php
+++ wp-content/themes/hmr-theme/templates/content-team-hero.php
@@ -12,7 +12,7 @@
 		    <div class="heff hover hastip"><a href="/team/bill-heffernan/"></a></div>
 		    <div class="ahr hover hastip"><a href="/team/brittanie-ahrens/"></a></div>
 		    <div class="ruben hover hastip"><a href="/team/burt-rubenstein/"></a></div>
-			<div class="mil hover hastip"><a href="/team/carli-milstein/"></a></div>
+			<!-- <div class="mil hover hastip"><a href="/team/carli-milstein/"></a></div> -->
 			<div class="eps hover hastip"><a href="/team/david-epstein/"></a></div>
 			<div class="grif hover hastip"><a href="/team/jessica-griffin/"></a></div>
 			<div class="hens hover hastip"><a href="/team/john-hensel/"></a></div>
