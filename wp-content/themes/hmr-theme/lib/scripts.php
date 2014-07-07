77fba0ac1007218470f02c1349cbd3ec4d23fbcb
Jon Bukiewicz <jonathan@tenebroso.net>
2014-07-07 12:00:18 -0500

    Fix homepage drop down menu

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'fd51adda574d105c3a6a0c9a61b6ba08');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a059cfdfa8a1d663412be81731e970ca');
 
   // Load style.css from child theme
   if (is_child_theme()) {
