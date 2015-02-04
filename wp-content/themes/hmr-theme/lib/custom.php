693fca2c315e9e564d4f2c0ed030962593f25771
Jon Bukiewicz <jonathan@tenebroso.net>
2015-02-04 09:18:34 -0600

    Add team member taxonomy

--- wp-content/themes/hmr-theme/lib/custom.php
+++ wp-content/themes/hmr-theme/lib/custom.php
@@ -58,6 +58,42 @@ function register_cpt_hmr() {
     
 }
 
+// Register Custom Taxonomy
+function team_type_tax() {
+
+    $labels = array(
+        'name'                       => _x( 'Types', 'Taxonomy General Name', 'text_domain' ),
+        'singular_name'              => _x( 'Type', 'Taxonomy Singular Name', 'text_domain' ),
+        'menu_name'                  => __( 'Type', 'text_domain' ),
+        'all_items'                  => __( 'All Types', 'text_domain' ),
+        'parent_item'                => __( 'Parent Type', 'text_domain' ),
+        'parent_item_colon'          => __( 'Parent Type:', 'text_domain' ),
+        'new_item_name'              => __( 'New Type Name', 'text_domain' ),
+        'add_new_item'               => __( 'Add New Type', 'text_domain' ),
+        'edit_item'                  => __( 'Edit Type', 'text_domain' ),
+        'update_item'                => __( 'Update Type', 'text_domain' ),
+        'separate_items_with_commas' => __( 'Separate typess with commas', 'text_domain' ),
+        'search_items'               => __( 'Search Types', 'text_domain' ),
+        'add_or_remove_items'        => __( 'Add or remove types', 'text_domain' ),
+        'choose_from_most_used'      => __( 'Choose from the most used types', 'text_domain' ),
+        'not_found'                  => __( 'Not Found', 'text_domain' ),
+    );
+    $args = array(
+        'labels'                     => $labels,
+        'hierarchical'               => true,
+        'public'                     => true,
+        'show_ui'                    => true,
+        'show_admin_column'          => true,
+        'show_in_nav_menus'          => true,
+        'show_tagcloud'              => true,
+    );
+    register_taxonomy( 'type', array( 'team' ), $args );
+
+}
+
+// Hook into the 'init' action
+add_action( 'init', 'team_type_tax', 0 );
+
 /* =============================================================================
    Capabilities CPT
    ========================================================================== */
