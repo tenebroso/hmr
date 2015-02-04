6406ca9024b31545badf3806ba3b974fde1e8d3a
Jon Bukiewicz <jonathan@tenebroso.net>
2015-02-04 12:10:47 -0600

    Fix black bar on blog post title focus

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3397e9933f80d39af234d5377c1a996c');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c6a2c754c1b6f532d961da1e0bf4abbc');
 
   // Load style.css from child theme
   if (is_child_theme()) {
