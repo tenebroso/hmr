ee0274dcca1f43c83de82d133ce3dec1ed175724
Jon Bukiewicz <jonathan@tenebroso.net>
2015-02-05 17:51:28 -0600

    Fixes to team grid

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '52b888b6167dd247917925bbc68ce131');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '33eaa1fa8f30e56c91ab834a7b4c99b8');
 
   // Load style.css from child theme
   if (is_child_theme()) {
