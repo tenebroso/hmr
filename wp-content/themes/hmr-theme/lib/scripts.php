ec55c0d5474cbdf1c22a99b3d43bc93042ba24e5
Jon Bukiewicz <jonathan@tenebroso.net>
2015-05-27 14:20:26 -0500

    Fix button color on archive/category pages

--- wp-content/themes/hmr-theme/lib/scripts.php
+++ wp-content/themes/hmr-theme/lib/scripts.php
@@ -12,7 +12,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '4dc60f811a6b8a4442388c0b0270b22b');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '317907f2dddbdaabde4d508d7a4a0c2e');
 
   // Load style.css from child theme
   if (is_child_theme()) {
